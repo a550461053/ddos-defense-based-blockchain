1c7b223f795db409d1acafc90555374e9dc2060a
baqiuxiao <baqiuxiao@163.com>
2018-02-04 19:29:21 +0800

    界面更改

--- angular-app/submit.sh
+++ angular-app/submit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.decentralized.energy.network.Energy", "energyID": "567889", "units": "12", "value": "12", "ownerID": "12", "ownerEntity": "Resident","description":"udp"}' 'http://localhost:3000/api/Energy'
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.decentralized.energy.network.Energy", "energyID": "56230", "units": "10", "value": "'"$(date +'%s')"'", "ownerID": "12", "ownerEntity": "Resident","description":"udp","flag":"0"}' 'http://localhost:3000/api/Energy'
 sleep 10
 #curl -X GET --header 'Accept: application/json' 'http://localhost:3000/api/Energy'
