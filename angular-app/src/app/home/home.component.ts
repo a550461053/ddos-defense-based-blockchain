ceb21a30f506a677dc5899c9965ea607abc58eb5
a550461053 <550461053@qq.com>
2018-02-08 17:53:58 +0800

    增加home界面的动态显示

--- angular-app/src/app/home/home.component.ts
+++ angular-app/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input , ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 
@@ -10,16 +10,20 @@ import 'rxjs/add/operator/toPromise';
 // import { Component, OnInit, Input } from '@angular/core';
 // import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { EnergyService } from '../Energy/Energy.service';
+import { DDoSService } from '../DDoS/DDoS.service';
 // import 'rxjs/add/operator/toPromise';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	// providers: [HomeService]
-  providers: [EnergyService]
+  providers: [EnergyService, DDoSService]
 })
 export class HomeComponent implements OnInit {
+
+
 
 	// constructor(){};
 
@@ -33,27 +37,42 @@ export class HomeComponent implements OnInit {
 	// 2. Energy
 	myForm: FormGroup;
 
-  private allAssets;
+  private allAssets;  // 此处指代energy
   private asset;
   private currentId;
 	private errorMessage;
 
-	energyID = new FormControl("", Validators.required);
-	units = new FormControl("", Validators.required);
-	value = new FormControl("", Validators.required);
-	ownerID = new FormControl("", Validators.required);
-	ownerEntity = new FormControl("", Validators.required);
+	// 定时器
+	private timer;
+
+	private allDDoS;
+	private ddos;
+
+	// energyID = new FormControl("", Validators.required);
+	// units = new FormControl("", Validators.required);
+	// value = new FormControl("", Validators.required);
+	// ownerID = new FormControl("", Validators.required);
+	// ownerEntity = new FormControl("", Validators.required);
 
 	// constructor(private serviceTransaction:HomeService, fb: FormBuilder,
-	constructor(private serviceEnergy:EnergyService, fb: FormBuilder) {
+	constructor(private serviceEnergy:EnergyService, fb: FormBuilder,
+							private serviceDDoS:DDoSService,
+							private ref: ChangeDetectorRef) {
 							this.myForm = fb.group({
-										energyID:this.energyID,
-										units:this.units,
-										value:this.value,
-										ownerID:this.ownerID,
-										ownerEntity:this.ownerEntity
+										// energyID:this.energyID,
+										// units:this.units,
+										// value:this.value,
+										// ownerID:this.ownerID,
+										// ownerEntity:this.ownerEntity
 
 							});
+
+							this.timer = setInterval(() => {
+								this.ref.detectChanges(); // 检测变化
+								this.loadAllEnergy();
+								this.loadAllDDoS();
+								// console.log('timer');
+							}, 2000)
 
 	};
 
@@ -62,7 +81,24 @@ export class HomeComponent implements OnInit {
 
 		// this.loadAllTransactions();
 		this.loadAllEnergy();
+		this.loadAllDDoS();
 	}
+
+	// 销毁组件时，清除定时器
+	ngOnDestroy() {
+		if (this.timer) {
+			clearInterval(this.timer);
+		}
+	}
+
+	// ngOnChanges(changes: SimpleChanges) {
+	// 	for (let pName in changes) {
+	// 		let ch = changes[pName];
+	// 		console.log(pName);
+	// 		console.log(ch);
+	// 	}
+	// 	console.log('noOnChanges');
+	// }
 
 	//sort the objects on key
 	// sortByKey(array, key): Object[] {
@@ -130,6 +166,29 @@ export class HomeComponent implements OnInit {
 				tempList.push(asset);
 			});
 			this.allAssets = tempList;
+		})
+		.catch((error) => {
+				if(error == 'Server error'){
+						this.errorMessage = "Could not connect to REST server. Please check your configuration details";
+				}
+				else if(error == '404 - Not Found'){
+				this.errorMessage = "404 - Could not find API route. Please check your available APIs."
+				}
+				else{
+						this.errorMessage = error;
+				}
+		});
+	}
+	loadAllDDoS(): Promise<any> {
+		let tempList = [];
+		return this.serviceDDoS.getAll()
+		.toPromise()
+		.then((result) => {
+			this.errorMessage = null;
+			result.forEach(ddos => {
+				tempList.push(ddos);
+			});
+			this.allDDoS = tempList;
 		})
 		.catch((error) => {
 				if(error == 'Server error'){
